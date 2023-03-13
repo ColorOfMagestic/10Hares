e9e21f14c5a0e02a99f68da2758f84a9640b0ba7
Kuznetsov Pavel <colorOfMagic@mail.ru>
2023-03-13 14:15:30 +0500

    Minor edits

--- app/js/src/slider.js
+++ app/js/src/slider.js
@@ -1,20 +1,14 @@
 
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.intro_banner-swiper', {
     loop: true,
+    spaceBetween: 10,
+    autoHeight: true,
     pagination: {
         el: '.swiper-pagination',
     },
     // Navigation arrows
     navigation: {
-        nextEl: `[data-role="next"]`,
-        prevEl: `[data-role="prev"]`,
+        nextEl: `[data-role="intro_banner-next"]`,
+        prevEl: `[data-role="intro_banner-prev"]`,
     },
-    breakpoints: {
-      320: {
-      },
-      768: {
-      },
-      1170: {
-      }
-    }
 });
